5fc893a2c515545ca0b8428283bed88cfd6fe3c3
Eunice Gatehi <eunicegatehi@gmail.com>
2017-10-23 02:13:22 +0300

    app initialization

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@
 from flask import Flask
 from app.userclass import User
 from app.recipes import Recipes
+from app.userclass import User 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
 user= User()
 recipes= Recipes()
+
 # Load the views
 from app import views
 
