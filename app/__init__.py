e52c31b3dac9297a956366d02181020e10a9e5e2
Eunice Gatehi <eunicegatehi@gmail.com>
2017-10-22 20:59:24 +0300

    [Feature]update objcts for app initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 # app/__init__.py
 
 from flask import Flask
-
+from app.userclass import User
+from app.recipes import Recipes
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
-
+user= User()
+recipes= Recipes()
 # Load the views
 from app import views
 
